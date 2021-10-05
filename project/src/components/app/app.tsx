116b2395bf28923275265928d431336faa3e7c7d
Alexander <orientstar96@gmail.com>
2021-10-05 20:25:52 +0500

    4.1 Добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import MainPage from '../main-page/main-page';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+import Movie from '../movie/movie';
+import AddReview from '../add-review/add-review';
+import Player from '../player/player';
 import type {AppMovieCardProps} from './type';
+import {AppRoutes} from '../../const';
+
 
 function App({promoMovieInfo}: AppMovieCardProps): JSX.Element {
   const {
@@ -8,7 +16,30 @@ function App({promoMovieInfo}: AppMovieCardProps): JSX.Element {
     genre,
   } = promoMovieInfo;
 
-  return <MainPage name={name} release={release} genre={genre} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoutes.MainPage}>
+          <MainPage name={name} release={release} genre={genre} />
+        </Route>
+        <Route exact path={AppRoutes.SignIn}>
+          <SignIn />
+        </Route>
+        <Route exact path={AppRoutes.MyList}>
+          <MyList />
+        </Route>
+        <Route exact path={AppRoutes.Movie}>
+          <Movie />
+        </Route>
+        <Route exact path={AppRoutes.AddReview}>
+          <AddReview />
+        </Route>
+        <Route exact path={AppRoutes.Player}>
+          <Player />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/const.ts
@@ -0,0 +1,8 @@
+export enum AppRoutes {
+MainPage = '/',
+SignIn = '/login',
+MyList = '/mylist',
+Movie = '/films/:id',
+AddReview = '/films/:id/review',
+Player = '/player/:id',
+}
