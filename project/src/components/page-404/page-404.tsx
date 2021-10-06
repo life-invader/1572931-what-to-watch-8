733a9d24ba4f0e314274617b41156b3de2329558
Alexander <orientstar96@gmail.com>
2021-10-06 20:46:35 +0500

    4.3 Стилизует компонент 404

--- project/src/components/page-404/page-404.tsx
+++ project/src/components/page-404/page-404.tsx
@@ -1,5 +1,11 @@
+import './page-404.css';
+
 function Page404(): JSX.Element {
-  return <h1>Такой страницы не существует</h1>;
+  return (
+    <div className="numberError">
+      <h1 className="error" data-text="404">404</h1>
+    </div>
+  );
 }
 
 export default Page404;
